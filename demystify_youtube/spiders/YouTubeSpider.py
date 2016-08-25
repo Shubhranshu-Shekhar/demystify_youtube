c325b5bfd0c7d594ac67f1dc1d6d3cf255827601
Shubhranshu Shekhar <shubhranshu.cse@gmail.com>
2016-08-25 19:20:35 +0530

    Youtube crawler iteration 0 completed

--- demystify_youtube/spiders/YouTubeSpider.py
+++ demystify_youtube/spiders/YouTubeSpider.py
@@ -17,6 +17,45 @@ class YouTubeSpider(scrapy.Spider):
         #This item is required to save all the data
         item = YoutubeItem()
 
-        video_title = hxs.xpath("//div[@id='watch-headline-title']//span[@class='watch-title']/@title").extract()[0]
+        try:
+            video_name = hxs.xpath("//div[@id='watch-headline-title']//span[@class='watch-title']/@title").extract()[0]
+            video_url = response.url
+
+            #description contains html tags as well because description has embedded links etc.
+            video_description = hxs.xpath("//div[@id='watch-description-text']//p[@id='eow-description']").extract()[0]
+            num_views = hxs.xpath("//div[@class='watch-view-count']/text()").extract()
+
+            user_sentiment = hxs.xpath("//div[@id='watch8-sentiment-actions']//button//span[@class='yt-uix-button-content']/text()").extract()
+            num_like = user_sentiment[0]
+            num_dislike = user_sentiment[2]
+
+            user = hxs.xpath("//div[@id='watch7-user-header']//div[@class='yt-user-info']/a/@href").extract()[0]
+            suggested_videos = list(set(hxs.xpath("//ul[@id='watch-related']//a/@href").extract()))
+            next_video = hxs.xpath("//div[@class='autoplay-bar']//ul[@class='video-list']//a/@href").extract()[0]
+            crawl_depth = str(response.meta['depth'])
+        except:
+            print "Some fields couldn't be parsed for " + response.url
+
+        #Populate our youtube item to be saved
+        item['video_url'] = video_url
+        item['video_name'] = video_name
+        item['video_description'] = video_description
+        item['num_views'] = num_views
+        item['num_likes'] = num_like
+        item['num_dislikes'] = num_dislike
+        item['user'] = user
+        item['suggested_videos'] = suggested_videos
+        item['next_video'] = next_video
+        item['crawl_depth'] =  crawl_depth
+
+        yield item
+
+
+
+
+
+
+
+
 
 
